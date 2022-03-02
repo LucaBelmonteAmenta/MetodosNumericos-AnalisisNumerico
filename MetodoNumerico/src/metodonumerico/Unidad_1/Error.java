61baee875f4331c24b6a4ea5f9953afa82e63d29
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2022-03-02 20:38:16 -0300

    Se re-crea nuevamente el proyecto
    Se borra y crea otra vez, pero en lugar de crearse con el gestor Maven, se emplea el gestor Ant, porque resulta mas fácil de manipular así.

---
+++ MetodoNumerico/src/metodonumerico/Unidad_1/Error.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodonumerico.Unidad_1;
+
+/**
+ *
+ * @author Lucas Belmonte
+ */
+public class Error {
+    
+}
