a2d24f8dc87e12d88a8695f961f46888e5c74d38
Lucas_Belmonte_Amenta <lucasbelmonteamenta2.0@gmail.com>
2022-03-02 21:24:45 -0300

    Se crea la clase Respuesta
    Se crea una clase para definir los objetos de devolverán todos los métodos del backend al frontend.
    Lo que se busca es estandarizar toda devolución de información al usuario.

---
+++ MetodoNumerico/src/metodonumerico/Respuesta.java
@@ -0,0 +1,35 @@
+
+package metodonumerico;
+
+
+public class Respuesta {
+    
+    public Respuesta () { 
+        
+    }
+    
+    public String resultadoFinal;
+    
+    public Double errorAproximado;
+    
+    public int numeroIteraciones;
+    
+    public Double[] arrayDouble1;
+    public Double[] arrayDouble2;
+    public Double[] arrayDouble3;
+    
+    public String[] arrayString1;
+    public String[] arrayString2;   
+    public String[] arrayString3;
+    
+    public Double[][] matriz1;
+    public Double[][] matriz2;
+    public Double[][] matriz3;
+    
+    public String[] nombresCamposTabla;
+    
+    public boolean fallo;
+    
+    public String descripcionFallo;
+    
+}
